3730339d7d2d6a11a781b97ef4c3b04a4a29ac71
Heitor <134107213+HeitorTeixeiradeMenezes@users.noreply.github.com>
2024-07-29 17:56:27 -0300

    dataTests

--- Carsh/tests/date.js
+++ Carsh/tests/date.js
@@ -1,2 +1,19 @@
-const now = new Date();
-console.log(now.type);
+const data = new Date();
+let formatarData = function(dataNaoFormatada){
+    const ano = dataNaoFormatada.getFullYear();
+    const mes = ("0" + (dataNaoFormatada.getMonth() + 1)).slice(-2);
+    const dia = ("0" + dataNaoFormatada.getDate()).slice(-2)
+    const dataFormatada = `${ano}-${mes}-${dia}`
+    return dataFormatada
+}
+function adicionarDias(data, dias) {
+    const novaData = new Date(data);
+    novaData.setDate(novaData.getDate() + dias);
+    return novaData;
+}
+
+
+
+// console.log(dataFormatada(data).getDay())
+// document.getElementById('solicitacao').value = formatarData(data);
+// document.getElementById('entrega').value = formatarData(adicionarDias(data, 30));
