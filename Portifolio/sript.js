cccba76d75849f55dc13ba231ce9ca70598ceb0c
Heitor <134107213+HeitorTeixeiradeMenezes@users.noreply.github.com>
2024-09-26 12:15:02 -0300

    protfolioupdate+carsh

--- Portifolio/sript.js
+++ Portifolio/sript.js
@@ -9,36 +9,42 @@ let mobiletags = function() {
     }
 }
 
-let traduzirPortugues = function(){
+let traduzirUS = function(){
     // Header
     let header = document.querySelector("header").querySelectorAll("div")
-    header[0].querySelector("p").innerHTML = "Pessoal"
-    header[3].querySelector("p").innerHTML = "Pessoal"
-    header[4].querySelectorAll("a")[0].innerHTML = "sobre"
-    header[4].querySelectorAll("a")[2].innerHTML = "currículo"
-    document.querySelector("header").querySelectorAll("a")[4].innerHTML = "Fale Comigo"
-    document.querySelector(".plinks").querySelectorAll("a")[0].innerHTML = "sobre"
-    document.querySelector(".plinks").querySelectorAll("a")[2].innerHTML = "currículo"
+    header[0].querySelector("p").innerHTML = "Personal"
+    header[3].querySelector("p").innerHTML = "Personal"
+    header[4].querySelectorAll("a")[0].innerHTML = "about"
+    header[4].querySelectorAll("a")[1].innerHTML = "projects"
+    header[4].querySelectorAll("a")[2].innerHTML = "curriculum"
+    document.querySelector("header").querySelectorAll("a")[4].innerHTML = "Lets talk"
+    document.querySelector(".plinks").querySelectorAll("a")[0].innerHTML = "about"
+    document.querySelector(".plinks").querySelectorAll("a")[1].innerHTML = "projects"
+    document.querySelector(".plinks").querySelectorAll("a")[2].innerHTML = "curriculum"
     // Main
-    document.querySelector(".hello").querySelector("h1").innerHTML = "Olá, bem-vindo Eu sou <b>Heitor Teixeira</b> Designer UI/UX- Desenvolvedor web front-end"
-    document.querySelector(".hire").querySelectorAll("a")[0].innerHTML = "Contrate"
-    document.querySelector(".hire").querySelectorAll("a")[1].innerHTML = "Ver Portfolio"
+    document.querySelector(".hello").querySelector("h1").innerHTML = "<h1>Hello welcome I’m <b>Heitor Teixeira </b>UI/UX Designer-Front-end Web Developer</h1>"
+    document.querySelector(".hire").querySelectorAll("a")[0].innerHTML = "Hire me"
+    document.querySelector(".hire").querySelectorAll("a")[1].innerHTML = "See portfolio"
     
-    document.querySelectorAll("h2")[0].innerHTML = "Sobre <b>mim</b>"
-    document.querySelector(".about").querySelector("p").innerHTML = `Estudante front-end na <b>Universidade Federal de Uberlândia</b>, cursando <b>Sistemas de Informação</b>. Apaixonado por criar experiências web intuitivas através de <b class="normal" style="color: rgba(230, 81, 0, 1);">HTML</b>, <b class="normal" style="color: rgba(31, 153, 237, 1);">CSS</b> e <b class="normal" style="color: rgba(228, 206, 0, 1);">JavaScript</b>. Comprometido em aprimorar habilidades em design e programação para inovar no desenvolvimento web.`;
+    document.querySelectorAll("h2")[0].innerHTML = "About <b>me</b>"
+    document.querySelector(".about").querySelector("p").innerHTML = `Front-end student at <b>Universidade Federal de Uberlândia</b>, studying <b>Information Systems</b>.
+                     Passionate about creating intuitive web experiences through <b class="normal" style="color: rgba(230, 81, 0, 1);">HTML</b>,
+                      <b class="normal" style="color: rgba(31, 153, 237, 1);">CSS</b>, and <b class="normal" style="color: rgba(228, 206, 0, 1);">JavaScript</b>. Committed
+                       to honing skills in design and scripting to innovate in web development.`
 
-    document.querySelectorAll("h2")[2].innerHTML = "Baixe o <b>currículo</b>"
+    document.querySelectorAll("h2")[1].innerHTML = "Projects"
+    document.querySelectorAll("h2")[2].innerHTML = "Download the <b>curriculum</b>"
 }
-let traduzirUS = function(){
+let traduzirBR = function(){
     const url = "https://heitorteixeirademenezes.github.io/personalProjects/Portifolio/Index.html"
     window.location.href = url
 }
 
 let verificar = function(){
     const valor = document.getElementById("traduzir").value
-    if(valor === "br"){
-        traduzirPortugues()
-    } else if(valor === "us"){
+    if(valor === "us"){
         traduzirUS()
+    } else if(valor === "br"){
+        traduzirBR()
     }
 }
